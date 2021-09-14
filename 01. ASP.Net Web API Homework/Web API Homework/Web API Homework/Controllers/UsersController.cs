f88f70aa2ae3fd2b99f001ed0f959151fa5efd7c
Viktor-Stojkov <viktor.stojkov93@gmail.com>
2021-09-14 16:02:38 +0200

    01.Homework

---
+++ 01. ASP.Net Web API Homework/Web API Homework/Web API Homework/Controllers/UsersController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_Homework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        [HttpGet] //http://localhost:[port]/api/users
+        public ActionResult<List<string>> GetAllNameFromStaticDbClass()
+        {
+            return StatusCode(StatusCodes.Status200OK, HomeworkStaticDb.UserNames);
+        }
+
+        [HttpGet("{userId}")] //http://localhost:[port]/api/users/0
+        public ActionResult<string> GetNameById(int userId)
+        {
+            try
+            {
+                if (userId < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "The index has negative value");
+                }
+                if (userId >= HomeworkStaticDb.UserNames.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no note with userId {userId} !");
+                }
+                return StatusCode(StatusCodes.Status200OK, HomeworkStaticDb.UserNames[userId]);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error occured");
+            }
+        }
+
+        [HttpPost] //http://localhost:[port]/homework.html
+        public IActionResult AddNewUserPostMethod()
+        {
+            try
+            {
+                using(StreamReader addUserStreamReader = new StreamReader(Request.Body))
+                {
+                    string addUser = addUserStreamReader.ReadToEnd();
+                    HomeworkStaticDb.UserNames.Add(addUser);
+                    return StatusCode(StatusCodes.Status201Created, "The note was created");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error occured");
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteExistingUser()
+        {
+            try
+            {
+                using (StreamReader DeleteUserStreamReader = new StreamReader(Request.Body))
+                {
+                    string deleteUser = DeleteUserStreamReader.ReadToEnd();
+                    int accessById = Int32.Parse(deleteUser);
+                    if (accessById < 0)
+                    {
+                        return StatusCode(StatusCodes.Status400BadRequest, "The index has negative value");
+                    }
+                    if (accessById >= HomeworkStaticDb.UserNames.Count)
+                    {
+                        return StatusCode(StatusCodes.Status404NotFound, $"There is no note with userId {accessById}");
+                    }
+                    HomeworkStaticDb.UserNames.RemoveAt(accessById);
+                    return StatusCode(StatusCodes.Status204NoContent, "The note was deleted");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error occured");
+            }
+        }
+    }
+}
---
+++ 01. ASP.Net Web API Homework/Web API Homework/Web API Homework/HomeworkStaticDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_Homework
+{
+    public static class HomeworkStaticDb
+    {
+        public static List<string> UserNames = new List<string>
+        {
+            "Vase",
+            "Vesna",
+            "Sandra",
+            "Viktor",
+            "Viktorija"
+        };
+    }
+}
